6294f927d68b62d353f904f742e9a35f96945cd5
wizard-kgalm <cameron43@comcast.net>
2013-07-17 10:06:50 -0700

    Added some personal commentary.

--- Magician/commands.php
+++ Magician/commands.php
@@ -1,26 +1,26 @@
 <?php
 include_once('modules/Magician/functions.php');
 switch($args[0]) {
-	case "passport":
+	case "passport":		// I don't even remember making this command. In fact, I'm pretty sure $dAmn->login() never took args. 
 		$config['token']['username']=$args[1];$config['token']['password']=$args[2];$config['bot']['username']=$args[1];$config['token']['password']=$args[2];
 			if($argsF==""){$dAmn->say($f."Please put your username and password to recieve your authtoken.",$c);return null;
 		}else if($args[2]==""){$dAmn->say("$from: Please put your password in the bot window.",$c); print "\nWhat is the password?\n";
 		$args[2] = trim(fgets(STDIN));if($args[2] !==""){$config['token']['password']=$args[2];} save_config('token');save_config('bot');}$dAmn->login($args[1],token());
 		break;
-	case "atswap":
+	case "atswap":			// This still exists, but there's no point to its existence, since you'd be using the bot to grab a token as it's illegal to ask someone to post it.
 		$config['bot']['username']=$args[1];$config['bot']['token']=$args[2];
 		if($argsF==""){$dAmn->say($f."Please put your username and authtoken to change logins.",$c);return null;
 		}else if($args[2]==""){$dAmn->say("$from: Sorry, but you must put both your username and authtoken to use this command.",$c);return null;}
 		if($args[2] !==""){$config['bot']['username']=$args[1];$config['bot']['token']=$args[2]; save_config('bot');} $dAmn->login($args[1],$args[2]);
 		break;
-	case "token":
+	case "token":			// Also still used, but with a far nicer result. Shame this version is so clunky too.
 		$config['token']['username']=$args[1];$config['token']['password']=$args[2];
 		if($argsF==""){$dAmn->say($f."Please put your username and password to recieve your authtoken.",$c);return null; 
 		}else if($args[2]==""){$dAmn->say("$from: Sorry, but you must put both your username and password to use this command.",$c);return null;}else 		if($args[2]=="%"){$dAmn->say("$from: Please put your password in the bot window.",$c); print "\nWhat is the password?\n";
 		$args[2] = trim(fgets(STDIN));if($args[2] !==""){$config['token']['password']=$args[2];} save_config('token');save_config('bot');}$dAmn->say($f.token(),$c);
 		break;
 	case "kickroll":
-		if(!isset($argsF)){ $dAmn->say($f . "Please say who to kickroll.", $c);
+		if(!isset($argsF)){ $dAmn->say($f . "Please say who to kickroll.", $c); // I'm almost certain this still exists exactly as is. 
 		}else if ($user->has($from,99)){
 			$dAmn->say("$argsE[1], Prepare to be Rolled." , $c); 
 			sleep(0);
@@ -36,23 +36,23 @@ NEVER GONNA RUN AROUND AND DESERT YOU
 NEVER GONNA MAKE YOU CRY
 NEVER GONNA SAY GOODBYE
 NEVER GONNA TELL YOU LIES AND HURT YOU :thumb82026411: <abbr title='()'>&#8238;");
-		}else $dAmn->kick($from,$c, "Trying to kickroll $argsF without the proper privs. :P");;break;
+		}else $dAmn->kick($from,$c, "Trying to kickroll $argsF without the proper privs. :P");break;
 
-	case "addons":
+	case "addons":	// These are out of date. I'm pretty sure all of those links are busted. 
 		$dAmn->say("<sup><abbr title=\"$from\"></abbr>:thumb103219269: Here are the addons! Start with <a href=\"http://addons.mozilla.org/en-US/firefox/downloads/latest/748\">Greasemonkey</a>, then after restarting, download <a href=\"http://nebulon.botdom.com/experiments/daxfix.user.js\">dAx</a>, <a href=\"http://damncolors.freehostia.com/dAmnColors.user.js\">dAmn Colors</a>, <a href=\"http://www.javascripthost.com/s1/bin/dAmnpwn.user.js\">dAmn.pwn</a>, <a href=\"http://www.jasonwhutchinson.com/GM/Emotes/emotescript.user.js\">Emotes Script</a>, <a href=\"http://testground2206.freehostia.com/damn_goodies.user.js\">dAmn Goodies</a>, and <a href=\"http://userstyles.org/styles/userjs/16505/dA%20Message%20Network%20-%20Sexify%20dAmn.user.js\">Sexify dAmn</a>.", $c);break;
-	case "shank":
+	case "shank":	// I made this command in my attempt to copy the (still) weirdly functioning version frozenpandaman had. It's such a waste of space. :')
 		if(!isset($argsF)){ $dAmn->say($f . "Please specify a user to shank!", $c);return; }
 		else
-		if ($user->has($from,99)){
+		if ($user->has($from,99)){								// You're an owner? Well, you may shank you you'd like!
 		$dAmn->say("$argsF: :stab: <b> >:C </b>" , $c);
 		}else
-		if ($argsF == $config['bot']['owner']) {
+		if ($argsF == $config['bot']['owner']) {				// Not an owner? You can't shank the hand that runs the bot.
 		$dAmn->say($f . "$argsF cannot be shanked! :noes:" , $c);return;   
 		}else
-			if ($argsF == $config['bot']['username']) {
+			if ($argsF == $config['bot']['username']) {			// You can't shank the bot, that's vandalism! 
 				$dAmn->say($f . "$argsF cannot be shanked! :noes:" , $c);return;
 		}else
-		$dAmn->say("$argsF: :stab: <b> D:<</b>" , $c);break;
+		$dAmn->say("$argsF: :stab: <b> D:<</b>" , $c);break;	// BARRING those two people, feel free to stick a knife into whomever's body you'd like.
 }
 
 ?>
--- Magician/functions.php
+++ Magician/functions.php
@@ -1,13 +1,13 @@
 <?php
-		function token(){ 
+		function token(){ 																// First of all, this is just as bad as the token grabber that was originally in Dante.
 		internalMessage( "Getting authtoken..." );
-		global $event,$config;
-		$UN=$config['token']['username']; $UP=$config['token']['password'];
+		global $event,$config;															// I'm fairly certain events shouldn't be called upon for this.
+		$UN=$config['token']['username']; $UP=$config['token']['password'];				// This is so ass backwards, I'm almost in tears at how round about I used to be.
 		$query = "ref=https%3A%2F%2Fwww.deviantart.com%2Fusers%2Flogin&username=" . $UN . "&password=" . $UP . "&reusetoken=1";
 
-		$a = fsockopen( "ssl://www.deviantart.com", 443, $errno, $errstr );
+		$a = fsockopen( "ssl://www.deviantart.com", 443, $errno, $errstr );				// Looking at this, I can see this would still actually work today, but only for the cookie.
 
-		fputs( $a, "POST /users/login HTTP/1.1\n" );
+		fputs( $a, "POST /users/login HTTP/1.1\n" );									// This isn't needed, as we have $dAmn->send_headers covering this bit.
 		fputs( $a, "Host: www.deviantart.com\n" );
 		fputs( $a, "User-Agent: " . BotUserAgent . "\n" );
 		fputs( $a, "Accept: text/html\n" );
@@ -25,11 +25,11 @@
 			$fullcookie = $full1[0] . ';';
 			$response = urldecode( $response );
 
-			if( stristr( $response, "Set-Cookie: " ) && stristr( $response, "authtoken" ) ) {
+			if( stristr( $response, "Set-Cookie: " ) && stristr( $response, "authtoken" ) ) {	// Tokens no longer included directly in your cookie, it allowed circumventing bans.
 				$bits = explode( "userinfo=", $response );
 				$cookie = substr( $bits[1], 0, strpos( $bits[1], "; expir" ) );
 				$cookie = unserialize( $cookie );
-				if( !empty( $cookie['authtoken'] ) ){
+				if( !empty( $cookie['authtoken'] ) ){										// lol this would be so much easier if that were the case.
 					$config['token']['token']=$cookie['authtoken']; save_config('token');
 					$config['bot']['token']=$cookie['authtoken']; save_config('bot');
 					internalMessage( "Authtoken: " . $cookie['authtoken'] );
@@ -38,17 +38,17 @@
 					return $cookie['authtoken'];
 					$config['bot']['token']=$cookie['authtoken']; save_config('bot');break;
 				 }else{
-					internalMessage( "Oops, couldn't get the authtoken. You may need to find it yourself." );$login = parse_ini_file( f( "config.ini" ) );$config['token']['password']=$login['password'];$config['token']['username']=$login['username'];save_config('token');$config['bot']['username']=$login['username'];$config['bot']['token']=token();save_config('bot');$event = "authtoken-fail";
+					internalMessage( "Oops, couldn't get the authtoken. You may need to find it yourself." );$login = parse_ini_file( f( "config.ini" ) );$config['token']['password']=$login['password'];$config['token']['username']=$login['username'];save_config('token');$config['bot']['username']=$login['username'];$config['bot']['token']=token();save_config('bot');$event = "authtoken-fail";					// Oh my god, this is so beyond sloppy. All that shit jumbled up on a SINGLE LINE? 
 					include f( "system/callables/event.php" );
 					return FALSE;
 				}
 			} else {
-				internalMessage( "Oops, couldn't get the authtoken. You may need to find it yourself." );$login = parse_ini_file( f( "config.ini" ) );$config['token']['password']=$login['password'];$config['token']['username']=$login['username'];save_config('token');$config['bot']['username']=$login['username'];$config['token']['token']=token();save_config('bot');$event = "authtoken-fail";
+				internalMessage( "Oops, couldn't get the authtoken. You may need to find it yourself." );$login = parse_ini_file( f( "config.ini" ) );$config['token']['password']=$login['password'];$config['token']['username']=$login['username'];save_config('token');$config['bot']['username']=$login['username'];$config['token']['token']=token();save_config('bot');$event = "authtoken-fail";							// LET'S COPY PASTE IT AGAIN!
 					include f( "system/callables/event.php" );
 					return FALSE;
 			}
 		} else {
-			internalMessage( "Oops, couldn't get the authtoken. You may need to find it yourself." );$login = parse_ini_file( f( "config.ini" ) );$config['token']['password']=$login['password'];$config['token']['username']=$login['username'];save_config('token');$config['bot']['username']=$login['username'];$config['bot']['token']=token();save_config('bot');$event = "authtoken-fail";
+			internalMessage( "Oops, couldn't get the authtoken. You may need to find it yourself." );$login = parse_ini_file( f( "config.ini" ) );$config['token']['password']=$login['password'];$config['token']['username']=$login['username'];save_config('token');$config['bot']['username']=$login['username'];$config['bot']['token']=token();save_config('bot');$event = "authtoken-fail";												// ROUND THREE. I like how it's basically the core's token grabber.
 					include f( "system/callables/event.php" );
 					return FALSE;
 		}break;
