514534258688e6cda157f32d321b3a54fd81358d
wizard-kgalm <cameron43@comcast.net>
2013-07-18 05:19:32 -0700

    cleaned up the old config for this obsolete version..

--- Magician/config.php
+++ Magician/config.php
@@ -5,33 +5,27 @@
 // Description: Few random commands, focus being on passport. 
 // Author: Wizard-Kgalm 
 // Version: 2.0
-// Notes: Completely redid the passport command, it now has support for saving alternate logins for the bot to change to at your command. Tripled the size of the commands file on it's own.
+// Notes: Remade passport, and added storage so the bot could save multiple logins to swap with at your command. Tripled the size of the commands file on it's own.
 // ====================
+//Activated on startup 
+$modules[$m] = new Module( $m, 1 ); 
+//Deactivated on startup
+//$modules[$m] = new Module($m,0); //Uncomment to use this option and then comment out the one about.
+$modules[$m]->setInfo( __FILE__ ); //NO TOUCHY or file will not load properly
 
-$modules[$m] = new Module($m,1); 
-//$modules[$m] = new Module($m,0);
-$modules[$m]->setInfo(__FILE__);
+// COMMANDS BELOW HERE.
+$modules[$m]->addCmd( 'store'   , 'commands', 99, 1 );
+$modules[$m]->addCmd( 'atswap'  , 'commands', 99, 1 );
+$modules[$m]->addCmd( 'show'    , 'commands', 99, 1 ); 
+$modules[$m]->addCmd( 'kickroll', 'commands', 75, 1 );
+$modules[$m]->addCmd( 'addons'  , 'commands', 0 , 1 );
+$modules[$m]->addCmd( 'shank'   , 'commands', 0 , 1 );
 
-
-$modules[$m]->addCmd('store','commands',99,1);
-$modules[$m]->addHelp('store','Usage: $config[\'bot\'][\'trigger\']store [logins |login |manual][add |del/remove |change |list |all]. Second brakcets deals with $config[\'bot\'][\'trigger\']store logins command. For more info, view "Magician README" included. ');
-
-//Unless you know anything about using authtokens, don't uncomment this command.
-//$modules[$m]->addCmd('atswap','commands',99,1);
-//$modules[$m]->addHelp("atswap","ATSwap Uses the supplied username and authtoken to change logins. Hint: You need to have the user's token before you can use this command. If you know the password, just use store.");
-
-//Again, unless you know how to use authtokens and cookies (explained in the readme to some extent), leave this disabled.
-//$modules[$m]->addCmd('show','commands',99,1);
-//$modules[$m]->addHelp('show','"Show" retrieves the authtoken or cookie for a specified username and password, supplied either from a stored ID number given to you from $config[\'bot\'][\'trigger\']store logins, or manually input using $config[\'bot\'][\'trigger\']show token\cookie manual.');
-
-$modules[$m]->addCmd('kickroll','commands',75,1);
-$modules[$m]->addHelp('kickroll','Actually created as a joke, this command is excellent for kicking in a hilarious way.');
-
-$modules[$m]->addCmd('addons','commands',0,1);
-$modules[$m]->addHelp('addons','Addons displays the popular addons for dAmn, as well as the client that runs them, Greasemonkey.');
-
-$modules[$m]->addCmd('shank','commands',0,1);
-$modules[$m]->addHelp('shank','Shank is a joke command merely used to stab someone.. :paranoid:.');
-
-
+// HELPS BELOW HERE.
+$modules[$m]->addHelp( 'store', "Usage: {$tr}store [logins |login |manual][add |del/remove |change |list |all]. Second brakcets deals with {$tr}store logins command. For more info, view the readme included." );
+$modules[$m]->addHelp( "atswap","ATSwap Uses the supplied username and authtoken to change logins. Hint: You need to have the user's token before you can use this command. If you know the password, just use store." );
+$modules[$m]->addHelp( 'show', "{$tr}show retrieves the authtoken or cookie for a specified username and password, supplied either from a stored ID number given to you from {$tr}store logins, or manually input using {$tr}show token\cookie manual." );
+$modules[$m]->addHelp( 'kickroll','Actually created as a joke, this command is excellent for kicking in a hilarious way.' );
+$modules[$m]->addHelp( 'addons', 'Addons displays the popular addons for dAmn, as well as the client that runs them, Greasemonkey.' );
+$modules[$m]->addHelp( 'shank', 'Shank is a joke command merely used to stab someone.. :paranoid:.' );
 ?>
